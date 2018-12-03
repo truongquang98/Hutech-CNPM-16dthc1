4ac33755c95a3f65f18f6061adaa1e8a0eae25ee
truongquang98 <43910186+truongquang98@users.noreply.github.com>
2018-12-03 11:57:20 +0700

    Update login.Designer.cs

--- Cafe_Free/Cafe_Free/login.Designer.cs
+++ Cafe_Free/Cafe_Free/login.Designer.cs
@@ -29,9 +29,13 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btThoat = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -41,50 +45,88 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(-3, -4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(627, 145);
+            this.panel1.Size = new System.Drawing.Size(773, 125);
             this.panel1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.White;
-            this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(134, 280);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 53);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "LOGIN";
-            this.button1.UseVisualStyleBackColor = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(219, 28);
+            this.label1.Location = new System.Drawing.Point(162, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(383, 97);
+            this.label1.Size = new System.Drawing.Size(460, 117);
             this.label1.TabIndex = 0;
             this.label1.Text = "CAFE FREE";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // button2
+            // button1
             // 
-            this.button2.BackColor = System.Drawing.Color.White;
-            this.button2.ForeColor = System.Drawing.Color.Black;
-            this.button2.Location = new System.Drawing.Point(268, 280);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(116, 53);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "LOGIN";
-            this.button2.UseVisualStyleBackColor = false;
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(189, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Đăng Nhập";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // btThoat
+            // 
+            this.btThoat.BackColor = System.Drawing.Color.White;
+            this.btThoat.ForeColor = System.Drawing.Color.Black;
+            this.btThoat.Location = new System.Drawing.Point(488, 280);
+            this.btThoat.Name = "btThoat";
+            this.btThoat.Size = new System.Drawing.Size(116, 53);
+            this.btThoat.TabIndex = 2;
+            this.btThoat.Text = "Thoát";
+            this.btThoat.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tên Đăng Nhập";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(37, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Mật Khẩu";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(221, 149);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(463, 27);
+            this.txtUser.TabIndex = 5;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(221, 202);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(463, 27);
+            this.txtPass.TabIndex = 6;
             // 
             // login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(624, 345);
+            this.ClientSize = new System.Drawing.Size(768, 345);
             this.ControlBox = false;
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btThoat);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Cursor = System.Windows.Forms.Cursors.Hand;
@@ -96,6 +138,7 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -104,6 +147,10 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btThoat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
     }
 }
